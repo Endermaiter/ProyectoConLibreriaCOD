16474a3bffd84176b99700b21a003199547878a2
Endermaiter <mpereznovoa@danielcastelao.org>
2022-02-18 10:56:11 +0100

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,11 @@
+package com.company;
+
+import libreriasv8.Consola;
+
+public class Main {
+
+    public static void main(String[] args) {
+	Consola miConsola = new Consola("Hola Mundo");
+    miConsola.visualiza();
+    }
+}
